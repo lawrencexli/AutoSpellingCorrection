eff9cadd7dc2278da7871edc4de6a1b285a65ccf
Sebastian Ascoli <sa036@bucknell.edu>
2020-11-12 14:00:35 -0500

    trie updated

--- trie.py
+++ trie.py
@@ -30,7 +30,43 @@ class Trie:
                 
         return cur.isEnd == True
                 
-            
+class Trie2(Trie):
+
+
+    def matchAll(self, word, freeSymbol='?'):
+        return self.__matchAllR(word, self.root, freeSymbol)
+
+    def __matchAllR(self, word, cur, freeSymbol):
+
+        words = []
+        
+        missing = word
+        explored = ""
+        loopBroken = False
+        for c in word:
+
+            missing = missing[1:]
+
+            if c == freeSymbol:
+                
+                for childVal, childNode in cur.children.items():
+                    newEnds = self.__matchAllR(missing, childNode, freeSymbol)
+                    
+                    for end in newEnds:
+                        words.append(explored + childVal + end)                    
+                
+                loopBroken = True
+                break
+                    
+
+            explored += c
+
+            if c in cur.children: cur = cur.children[c]
+            else: return []
+
+        if not loopBroken and cur.isEnd: words += [word]
+        
+        return words
         
 
 class Node:
