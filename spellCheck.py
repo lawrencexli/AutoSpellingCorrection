5905fc6c1c8cefd914842c07f56f478ce100d479
Sebastian Ascoli <sa036@bucknell.edu>
2020-11-12 16:42:42 -0500

    update

---
+++ spellCheck.py
@@ -0,0 +1,57 @@
+from trie import Trie
+
+def loadWordsTrie():
+    """
+    Load words from wordlist.txt into trie
+    """
+
+    f = open('wordlist.txt', 'r') 
+    lines = f.readlines()
+    f.close()
+
+    trie = Trie()
+    for line in lines:
+        word = line.rstrip() #remove '\n'
+        if word == "": continue #slip empty strings
+        trie.insert(word)
+
+    return trie
+
+
+def getWordsDistance1(word, freeSymbol='?'):
+    """
+    Get list of words at an edit distance of 1
+    """
+
+
+    possibleWords = []
+
+    #Insert
+    for i in range(0, len(word)+1):
+        newWord = word[:i] + freeSymbol + word[i:]
+        possibleWords.append(newWord)
+
+    #remove 
+    for i in range(0, len(word)):
+        newWord = word[0:i] + word[i+1:]
+        possibleWords.append(newWord)
+
+    #replace
+    for i in range(0, len(word)):
+        newWord = word[0:i] + freeSymbol + word[i+1:]
+        possibleWords.append(newWord)
+
+
+    return possibleWords
+
+
+def getWordsDistance(word, d, freeSymbol='?'):
+    """
+    Get list of words at an edit distance of d
+    """
+
+    if d==1: return getWordsDistance1(word, freeSymbol)
+    wordsPrev = getWordsDistance(word, d-1, freeSymbol)
+    words = []
+    for w in wordsPrev: words += getWordsDistance1(w, freeSymbol)
+    return words
